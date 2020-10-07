b54e250e00f8bd7e994a7a2c64986c4c22328684
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-07 14:11:17 -0400

    added angles to servo

--- main.ts
+++ main.ts
@@ -1,10 +1,22 @@
+let potentiometer = 0
 // Created by: Nina Mathew
 // Created on: 10/07/20
 // This program attaches a servo to the microbit.
 basic.forever(function () {
-    let potentiometer = 0
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
     basic.pause(1000)
+    potentiometer = pins.analogReadPin(AnalogPin.P1)
     basic.showNumber(potentiometer)
-    servos.P1.setAngle(potentiometer)
+    servos.P2.setRange(0, 180)
+    if (potentiometer <= 10) {
+        let servo_angle = 0
+        servos.P2.setAngle(0)
+        basic.showNumber(servo_angle)
+    }
+    if (potentiometer <= 500) {
+        servos.P2.setAngle(90)
+    }
+    if (potentiometer > 500) {
+        servos.P2.setAngle(180)
+    }
 })
