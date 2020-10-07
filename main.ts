4d5782e6d55e5545fbe8eb3967005572ec0ea239
Nina-Mathew <71400436+Nina-Mathew@users.noreply.github.com>
2020-10-07 14:41:16 -0400

    showed it on microbit

--- main.ts
+++ main.ts
@@ -1,4 +1,5 @@
 let potentiometer = 0
+let servo_angle = 0
 // Created by: Nina Mathew
 // Created on: 10/07/20
 // This program attaches a servo to the microbit.
@@ -6,17 +7,22 @@ basic.forever(function () {
     basic.showNumber(pins.analogReadPin(AnalogPin.P1))
     basic.pause(1000)
     potentiometer = pins.analogReadPin(AnalogPin.P1)
+    servo_angle = potentiometer / 180
     basic.showNumber(potentiometer)
     servos.P2.setRange(0, 180)
     if (potentiometer <= 10) {
-        let servo_angle = 0
         servos.P2.setAngle(0)
+        basic.showNumber(potentiometer)
         basic.showNumber(servo_angle)
     }
     if (potentiometer <= 500) {
         servos.P2.setAngle(90)
+        basic.showNumber(potentiometer)
+        basic.showNumber(servo_angle)
     }
     if (potentiometer > 500) {
         servos.P2.setAngle(180)
+        basic.showNumber(potentiometer)
+        basic.showNumber(servo_angle)
     }
 })
